8f7f06267eec661a0a84ba40b772e6ce749a43b5
Larissa Gomez Schmidt <schmidttein@gmail.com>
2025-06-12 12:03:26 -0300

    Novos Exercícios

--- Ex_2.2.1.py
+++ Ex_2.2.1.py
@@ -1,26 +1,14 @@
-frete_fixo = 3
-frete_adicional = 0.75
+import math
 
 while True:
     try:
-        preco_livro = float(input('Qual o preço do livro? R$'))
-        desconto = float(input('Quantos % de desconto a livraria ganha nessa compra? (ex: 20 para 20%) '))
-        quantidade_atacado = int(input('Quantas unidades serão pedidas? '))
-        
-        porcentagem = float(desconto)/100
-        preco_com_desconto = (preco_livro * quantidade_atacado) * (1 - porcentagem)
-        frete_total = frete_fixo + ((quantidade_atacado - 1) * frete_adicional)
-        custo_total = frete_total + preco_com_desconto
+        print('\nVamos calcular o volume de esferas para você!')
 
-        if quantidade_atacado > 1:
-            print(f'\nSabemos que o valor do frete é R${frete_fixo} mais R${frete_adicional} por exemplar. O frete total para essa compra é R${frete_total}')
-            print(f'\nO custo total dessa compra é R${custo_total}')
-            break
-        elif quantidade_atacado == 1:
-            print(f'O valor total do frete é R${frete_fixo}')
-            print(f'O valor total da compra é R${preco_livro+frete_fixo}')
-            break
-        else:
-            print('Esse não é um valor válido de unidades')
+        raio = float(input('\nDigite o raio da esfera: '))
+
+        volume = (4/3) * math.pi * (raio ** 3)
+
+        print(f'O volume da esfera com raio {raio} é {volume:.2f}')
+        exit()
     except Exception:
-        print('\nVocê precisa digitar um número válido.')
+        print('\nDigite apenas números!')
---
+++ Ex_2.2.2.py
@@ -0,0 +1,26 @@
+frete_fixo = 3
+frete_adicional = 0.75
+
+while True:
+    try:
+        preco_livro = float(input('Qual o preço do livro? R$'))
+        desconto = float(input('Quantos % de desconto a livraria ganha nessa compra? (ex: 20 para 20%) '))
+        quantidade_atacado = int(input('Quantas unidades serão pedidas? '))
+        
+        porcentagem = float(desconto)/100
+        preco_com_desconto = (preco_livro * quantidade_atacado) * (1 - porcentagem)
+        frete_total = frete_fixo + ((quantidade_atacado - 1) * frete_adicional)
+        custo_total = frete_total + preco_com_desconto
+
+        if quantidade_atacado > 1:
+            print(f'\nSabemos que o valor do frete é R${frete_fixo} mais R${frete_adicional} por exemplar. O frete total para essa compra é R${frete_total}')
+            print(f'\nO custo total dessa compra é R${custo_total}')
+            break
+        elif quantidade_atacado == 1:
+            print(f'O valor total do frete é R${frete_fixo}')
+            print(f'O valor total da compra é R${preco_livro+frete_fixo}')
+            break
+        else:
+            print('Esse não é um valor válido de unidades')
+    except Exception:
+        print('\nVocê precisa digitar um número válido.')
